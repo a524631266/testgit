39b882b310e6c200bec393882da221db8c5a4c88
navigatezll <navigatezll@didichuxing.com>
2021-01-03 10:38:32 +0800

    ad

---
+++ JavaTest1.java
@@ -0,0 +1,10 @@
+package com.xiaoju.daas.metastore.app.rest.resources;
+
+/**
+ * @Classname JavaTest1
+ * @Description TODO
+ * @Date 2021/1/3 10:37 上午
+ * @Created by navigate zhang
+ */
+public class JavaTest1 {
+}
